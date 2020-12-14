534ee5ba7f66791e6901d11f73b768b6efcf6b19
Andrii-Vekker <vekker304@gmail.com>
2020-12-15 00:26:12 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import './styles.css';
-
-
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -14,3 +6,32 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener('click', handleOnStartBtnClick);
+refs.stopBtn.addEventListener('click', handleOnStopBtnClick);
+
+let id = null;
+
+function handleOnStartBtnClick() {
+  id = setInterval(setBodyBackgroundColor, 1000);
+}
+
+function handleOnStopBtnClick() {
+  clearInterval(id);
+  id = null;
+}
+
+function setBodyBackgroundColor() {
+  refs.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+
+function randomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
